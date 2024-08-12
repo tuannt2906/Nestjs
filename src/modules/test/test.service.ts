1c27e64d21dbf4beeda5ed3b5089f5d26a6a99da
tuannt2906 <nguyentuan123.yeah@gmail.com>
2024-08-12 08:59:23 +0700

    test adding

--- src/modules/test/test.service.ts
+++ src/modules/test/test.service.ts
@@ -9,21 +9,15 @@ export class TestService extends CommonService<any,any>{
      this.data = [
        {
          id: 1,
-         username: "alex palestine",
+         name: "Dog",
          password: "hashed_password1",
-         email: "alex.p@example.com",
-         gender: "male",
-         phonenumber: "+1234567890",
-         age: 18
+        
        },
        {
          id: 2,
          username: "robert adam",
-         password: "hashed_password2",
-         email: "robert.a@example.com",
-         gender: "male",
-         phonenumber: "+0987654321",
-         age: 19
+      
        },
      ]
-    }}
+    }
+}
