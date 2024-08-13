01d1e7ae73b1d167056891e2ee04c0cac0d15e2e
tuannt2906 <nguyentuan123.yeah@gmail.com>
2024-08-13 09:03:11 +0700

    add password required

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -22,6 +22,9 @@ export class UserDTO {
   @IsNotEmpty()
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters long!' })
+  @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, { 
+    message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.' 
+  })
   password: string;
 
   @IsNotEmpty()
